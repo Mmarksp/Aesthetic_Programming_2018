149517598fc59de8264affe02dcfc7d8f42d1513
[Mmarksp] <[mark@tomani.dk]>
2018-03-19 15:15:36 +0100

    Update mini_ex6

--- mini_exercises/mini_ex6/fixation_and_changer.js
+++ mini_exercises/mini_ex6/fixation_and_changer.js
@@ -1,13 +1,11 @@
-//Do note that changing the shape from ellipse to rect, and the other way around as well, can change the experience a lot
-
-class fixation {
+class fixation { //colour of object, opaqueness, x-position, y-position, size (width and height), angle of moving direction, colour values for interaction with changers.
   constructor(colour, alpha, x, y, sz, angle, red, green, blue) {
     this.col = colour;
     this.alph = alpha;
     this.pos = new createVector(x, y);
     this.sz = sz;
     this.angle = angle;
-    this.vel = new createVector(0, 0);
+    this.vel = new createVector(0, 0); //The speed in a direction that I then add to the this.pos later
 
     this.re = red;
     this.gre = green;
@@ -16,20 +14,24 @@ class fixation {
 
   show() {
     fill(this.col, this.alph);
-    rect(this.pos.x, this.pos.y, this.sz, this.sz); //Change this to rectangle!!
+    if (toggle_fixation == true) { //Boolean variable determining its shape.
+      ellipse(this.pos.x, this.pos.y, this.sz, this.sz);
+    } else {
+      rect(this.pos.x, this.pos.y, this.sz, this.sz);
+    }
   }
 
-  setMovement() {
+  setMovement() { //A vector is defined as force. It has a fixed angle.
     let force = p5.Vector.fromAngle(radians(this.angle));
-    force.mult(map(this.sz, 0, 150, 4, 0.1));
-    this.vel.add(force);
+    force.mult(map(this.sz, 0, 150, 5, 0.1)); //This force is multiplied with values stemming from 4 to 0.1. The value is determined by the size of the object. Big = slow. Small = fast.
+    this.vel.add(force); //The vector speed and angle is added to the initial vector. Makes me think if couldn't just leave out creating the "force"-variable and going directly with this.vel.
   }
 
   movement() {
-    this.pos.add(this.vel)
+    this.pos.add(this.vel) //The final setMovement is then added to the position of the object, making the object move.
   }
 
-  edge() {
+  edge() { //Going over an edge moves you to the opposite side.
     if (this.pos.x > width + this.sz) {
       this.pos.x = -this.sz
     } else if (this.pos.x < -this.sz) {
@@ -54,17 +56,22 @@ class changer {
     this.sz = sz;
   }
 
-  setIntercept(index) {
-    for(let j = 0; j < fixations.length; j++) {
-      let d = dist(changers[index].x, changers[index].y, fixations[j].pos.x, fixations[j].pos.y)
-      if (d < fixations[j].sz) {
-        fill(fixations[j].re, fixations[j].gre, fixations[j].blu, changers[index].alph);
+  setIntercept(index) { //The index number of changers[] as argument here is used to get all of the changers through this function.
+    for(let j = 0; j < fixations.length; j++) { //Goes through all of the drawn fixations. I think this especially makes the program demanding on computers. Every changer needs to check for all of the fixations for 30 times a second (framerate).
+      let d = dist(changers[index].x, changers[index].y, fixations[j].pos.x, fixations[j].pos.y) //The dist is the distance between location of the changer and the fixation.
+      if (d < changers[index].sz/2 + fixations[j].sz/2) { //This is only true if those two objects are intersecting one another. Half the size is because the ojects are drawn from the CENTER.
+        fill(fixations[j].re, fixations[j].gre, fixations[j].blu, changers[index].alph); //Every fixation brings its own colour!
       }
-    }
-  }
+    } // Very interestingly, it is all of the changers that are affected if just one fixation is touching a changer. This was unexpected, but ultimately proved to be something I wanted.
+  } //Furthermore, this is just me guessing, but I think the reason why not all changers then turn only one colour even if many fixations are touching, is related to the query of changers as they are drawn.
+    //If a fixation with blue colour touches changer[1], and a fixation with green colour touches changer[200], then changer[1] through changer [199] will be blue, and the rest will be green. This is just me guessing.
 
   show() {
     noStroke();
-    rect(this.x, this.y, this.sz, this.sz); //Change this to rectangle!!
+    if (toggle_changer == true) {
+      rect(this.x, this.y, this.sz, this.sz);
+    } else {
+      ellipse(this.x, this.y, this.sz, this.sz);
+    }
   }
 }
--- mini_exercises/mini_ex6/mini_ex6.js
+++ mini_exercises/mini_ex6/mini_ex6.js
@@ -1,66 +1,75 @@
-new p5();
+new p5(); //I need to write this in order to utilize random() outside of a function
 
-var fixations = [];
+var fixations = []; //These are my two objects named after some characteristics about them
 var changers = [];
 
-var bg_col = random(0, 255);
+var background_colour = random(255);
 
 var fixations_amount = [3, 5, 10, 15, 30];
-var fixations_size = [5, 10, 30, 100];
+var fixations_size = [5, 10, 30, 100, 150];
 var fixation_colour = [0, 55, 100, 155, 200, 255];
-var fixation_alpha = [0, 20, 100, 200, 255];
+var fixation_alpha = [20, 60, 100, 150, 200, 255];  //I have named arrays of setups that I want my program to be able to choose from.
 
-var changers_amount = [5, 10, 50, 75, 100, 500, 750];
-var changers_size = [25, 50, 100, 100, 150, 250];
-var changers_alpha = [10, 20, 20, 30, 40, 50, 100, 150, 255];
+var changers_amount = [25, 50, 75, 100, 250, 500, 750, 950]; //If you have trouble running my program, try removing the highest number in size and amount of both objects.
+var changers_size = [25, 50, 100, 100, 150];
+var changers_alpha = [30, 40, 50, 100, 150, 230];
 
-
-
-var fix_am = random(fixations_amount);
-var fix_sz = random(fixations_size);
-var fix_col = random(fixation_colour);
-var fix_alph = random(fixation_alpha);
-
-var chan_am = random(changers_amount);
-var chan_sz = random(changers_size);
-var chan_alph = random(changers_alpha);
+var toggle_fixation = Math.random() >= 0.5; //credit: https://gist.github.com/pavelbinar/6333285
+var toggle_changer = Math.random() >= 0.5; //This gives me a random true/false-value for every reload of the page. This them determines, whether the shapes will be squares or circles.
 
 
 
+function setup() { //I have started by defining a variable for a random value of all of the arrays.
+  var fix_am = random(fixations_amount);
+  var fix_sz = random(fixations_size);
+  var fix_col = random(fixation_colour);
+  var fix_alph = random(fixation_alpha);
 
+  var chan_am = random(changers_amount);
+  var chan_sz = random(changers_size);
+  var chan_alph = random(changers_alpha);
 
-
-function setup() {
   createCanvas(windowWidth, windowHeight);
-  ellipseMode(RADIUS);
+  ellipseMode(CENTER);
   rectMode(CENTER);
+  frameRate(30); //fixing a reduced framerate improves the program, as it was very demanding on my computer
 
 
-  for(let i = 0; i < fix_am; i++) {
+  for(let i = 0; i < fix_am; i++) { //I first create a loop making ready for a specified amount of objects. The colour values in the last three parameters are meant for colouring the changers.
     fixations[i] = new fixation(fix_col, fix_alph, random(width), random(height), random(fix_sz), random(0, 360), random(255), random(255), random(255));
-    fixations[i].setMovement()
+    fixations[i].setMovement() //Here I fix a movement speed in a given direction.
   }
 
-  for(let i = 0; i < chan_am; i++) {
+  for(let i = 0; i < chan_am; i++) { //Similarly with the other class of object
     changers[i] = new changer(random(255), chan_alph, random(width), random(height), random(chan_sz));
-    fill(changers[i].col, changers[i].alph)
+    fill(changers[i].col, changers[i].alph) //I also state an initial colour prior to being interacted with by the "fixations".
   }
 }
 
 
 function draw() {
-  background(bg_col);
+  background(background_colour);
 
   for(let i = 0; i < fixations.length; i++) {
     push()
-    fixations[i].edge();
-    fixations[i].movement();
-    fixations[i].show();
+    fixations[i].edge(); //Going over the edge of the canvas means ending up at the opposite edge.
+    fixations[i].movement(); //Here I then continuously draw the previously fixed speed in a fixed direction.
+    fixations[i].show(toggle_fixation); //The argument here is to determine the shape from the other js-file.
     pop()
   }
 
   for (let i = 0; i < changers.length; i++) {
-    changers[i].setIntercept(i);
-    changers[i].show();
+    changers[i].setIntercept(i); //I continuously determine if a fixation is touching a changer. Funny unanticipated results happened through the use of such function.
+    changers[i].show(toggle_changer);
+  }
+}
+
+
+function keyPressed() { //Ability for the user to change the shape of the objects, while the sketch is running.
+  if (keyCode == 90) { //Z
+    toggle_fixation = !toggle_fixation;
+  }
+  if (keyCode == 88) { //X
+    toggle_changer = !toggle_changer;
   }
 }
