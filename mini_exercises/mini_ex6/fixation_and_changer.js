b9ac6d418c8bb8fa59f6b24c437b7ead2524744a
[Mmarksp] <[mark@tomani.dk]>
2018-03-19 02:08:13 +0100

    mini_ex6

---
+++ mini_exercises/mini_ex6/fixation_and_changer.js
@@ -0,0 +1,70 @@
+//Do note that changing the shape from ellipse to rect, and the other way around as well, can change the experience a lot
+
+class fixation {
+  constructor(colour, alpha, x, y, sz, angle, red, green, blue) {
+    this.col = colour;
+    this.alph = alpha;
+    this.pos = new createVector(x, y);
+    this.sz = sz;
+    this.angle = angle;
+    this.vel = new createVector(0, 0);
+
+    this.re = red;
+    this.gre = green;
+    this.blu = blue;
+  }
+
+  show() {
+    fill(this.col, this.alph);
+    rect(this.pos.x, this.pos.y, this.sz, this.sz); //Change this to rectangle!!
+  }
+
+  setMovement() {
+    let force = p5.Vector.fromAngle(radians(this.angle));
+    force.mult(map(this.sz, 0, 150, 4, 0.1));
+    this.vel.add(force);
+  }
+
+  movement() {
+    this.pos.add(this.vel)
+  }
+
+  edge() {
+    if (this.pos.x > width + this.sz) {
+      this.pos.x = -this.sz
+    } else if (this.pos.x < -this.sz) {
+      this.pos.x = width + this.sz
+    }
+    if (this.pos.y > height + this.sz) {
+      this.pos.y = -this.sz
+    } else if (this.pos.y < -this.sz) {
+      this.pos.y = height + this.sz
+    }
+  }
+}
+
+
+
+class changer {
+  constructor(col, alph, x, y, sz) {
+    this.col = col;
+    this.alph = alph;
+    this.x = x;
+    this.y = y;
+    this.sz = sz;
+  }
+
+  setIntercept(index) {
+    for(let j = 0; j < fixations.length; j++) {
+      let d = dist(changers[index].x, changers[index].y, fixations[j].pos.x, fixations[j].pos.y)
+      if (d < fixations[j].sz) {
+        fill(fixations[j].re, fixations[j].gre, fixations[j].blu, changers[index].alph);
+      }
+    }
+  }
+
+  show() {
+    noStroke();
+    rect(this.x, this.y, this.sz, this.sz); //Change this to rectangle!!
+  }
+}
---
+++ mini_exercises/mini_ex6/mini_ex6.js
@@ -0,0 +1,65 @@
+new p5();
+
+var fixations = [];
+var changers = [];
+
+var bg_col = random(0, 255);
+
+var fixations_amount = [3, 5, 10, 15, 30];
+var fixations_size = [5, 10, 30, 100];
+var fixation_colour = [0, 55, 100, 155, 200, 255];
+var fixation_alpha = [0, 20, 100, 200, 255];
+
+var changers_amount = [5, 10, 50, 75, 100, 500, 750];
+var changers_size = [25, 50, 100, 100, 150, 250];
+var changers_alpha = [10, 20, 20, 30, 40, 50, 100, 150, 255];
+
+
+
+var fix_am = random(fixations_amount);
+var fix_sz = random(fixations_size);
+var fix_col = random(fixation_colour);
+var fix_alph = random(fixation_alpha);
+
+var chan_am = random(changers_amount);
+var chan_sz = random(changers_size);
+var chan_alph = random(changers_alpha);
+
+
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  ellipseMode(RADIUS);
+
+
+  for(let i = 0; i < fix_am; i++) {
+    fixations[i] = new fixation(fix_col, fix_alph, random(width), random(height), random(fix_sz), random(0, 360), random(255), random(255), random(255));
+    fixations[i].setMovement()
+  }
+
+  for(let i = 0; i < chan_am; i++) {
+    changers[i] = new changer(random(255), chan_alph, random(width), random(height), random(chan_sz));
+    fill(changers[i].col, changers[i].alph)
+  }
+}
+
+
+function draw() {
+  background(bg_col);
+
+  for(let i = 0; i < fixations.length; i++) {
+    push()
+    fixations[i].edge();
+    fixations[i].movement();
+    fixations[i].show();
+    pop()
+  }
+
+  for (let i = 0; i < changers.length; i++) {
+    changers[i].setIntercept(i);
+    changers[i].show();
+  }
+}
