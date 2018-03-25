eed7c4659dcb4e03303c1a1b4c1f66b62795e4ec
[Mmarksp] <[mark@tomani.dk]>
2018-03-25 16:51:49 +0200

    Added a 6.1 - just added sound within sketch

---
+++ mini_exercises/mini_ex7/mini_ex7.js
@@ -0,0 +1,18 @@
+var data, s, txt;
+
+
+function preload() {
+  data = loadStrings("words.txt");
+}
+
+function setup() {
+  createP(data);
+}
+
+function draw() {
+
+}
+
+function mouseHover() {
+  
+}
