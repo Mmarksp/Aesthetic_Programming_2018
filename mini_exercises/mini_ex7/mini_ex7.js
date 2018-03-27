8cf59a769c9a1ef4f58c2f1fb308a4e206061b11
[Mmarksp] <[mark@tomani.dk]>
2018-03-27 11:46:51 +0200

    Added HTML for mini_ex6.1

--- mini_exercises/mini_ex7/mini_ex7.js
+++ mini_exercises/mini_ex7/mini_ex7.js
@@ -1,18 +1,58 @@
-var data, s, txt;
-
+var txt;      //variabler: txt bliver en array, words er objekter, j er increment og cycleNum er modolu
+var words = [];
+var j = 0;
+var cycleNum = 30;
 
 function preload() {
-  data = loadStrings("words.txt");
+  txt = loadStrings("words.txt");
 }
 
 function setup() {
-  createP(data);
+  createCanvas(windowWidth, windowHeight);
+  let col = random(0, 255); //startvaerdi for farve. Den aendrer sig laengere nede.
+  let alpha = 255;
+  textSize(72);
+
+  for(let i = 0; i < txt.length; i++) {
+    words[i] = new ord(txt[i], random(24, 100), col, alpha, width/2, height/2);  // Det er width/2 og height/2 som skal indstilles til musikken
+  } //Alle arrayenheder fra word.txt bliver transformeret til et objekt med argumenter for det enkelte objekt.
 }
 
 function draw() {
-
+  words[j].display();
+  words[j].fixTimeLoop(cycleNum); //Callback med variabel. FixTimeLoop er alle funktioner, der er afhaengige af modolu.
 }
 
-function mouseHover() {
-  
+
+
+class ord {
+  constructor(text, textSz, col, alpha, x, y) { //Vi lader alpha staa, hvis du vil bruge den til musikken. Ellers cutter vi den ud.
+    this.pos = new createVector(x, y); //Har ikke nogen anden betydning. Huk at henvise med "this/word[i]".pos.x/y.
+    this.text = text;
+    this.textSz = textSz;
+    this.col = col;
+    this.alpha = alpha;
+  }
+
+  setPosition() {
+    // position, numberOfTextElement       Denne skal nok henvises gennem draw
+  }
+
+  display() {
+      background(0);
+      this.col = random(0, 255);
+      // this.alpha = random(200, 255);
+      fill(this.col, this.alpha);
+      textAlign(CENTER)
+      textSize(this.textSz);
+      text(this.text, this.pos.x, this.pos.y);
+  }
+
+  fixTimeLoop(num) { //NO TOUCH. Aaah. Hvis du vil have noget slaaet sammen med oscillationen, saa put det ind her. Den koeres igennem draw.
+    let n = frameCount % num;
+    if (n == 0) {
+      background(50);
+      j++ //Det er random tekst. Den koerer i raekkefoelge.
+    }
+  }
 }
